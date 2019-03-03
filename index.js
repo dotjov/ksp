ba6bb054939ad8cb658009396718d4620e21719a
dotjov <dotjov@gmail.com>
2019-03-03 21:14:58 +0100

    Fix if ksp is undefined & throw

--- index.js
+++ index.js
@@ -1,23 +1,15 @@
 const sls = {}
 
 module.exports.createKsp = function (name, work) {
-    if (typeof name !== 'string') {
-        console.error('The function `createKsp` need a string to the 1st parameter!')
-        process.exit(0)
-    }
-    if (typeof work !== 'function') {
-        console.error('The function `createKsp` need a function to the 2nd parameter!')
-        process.exit(0)
-    }
+    if (typeof name !== 'string') throw new TypeError('The function `createKsp` need a string to the 1st parameter!')
+    if (typeof work !== 'function') throw new TypeError('The function `createKsp` need a function to the 2nd parameter!')
 
     sls[name] = work
 }
 
 module.exports.runKsp = function (name, ...args) {
-    if (typeof name !== 'string') {
-        console.error('The function `runKsp` need a string to the 1st parameter!')
-        process.exit(0)
-    }
+    if (typeof name !== 'string') throw new TypeError('The function `runKsp` need a string to the 1st parameter!')
+    if (sls[name] == null || sls[name] === undefined) throw new TypeError('The Ksp ' + name + ' is undefined!')
 
     let func = sls[name].apply(this, args)
     if (func instanceof Promise) {
