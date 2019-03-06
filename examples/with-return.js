5ec4b5ed52cf985cd32be917448b34b04b6f458c
dotjov <dotjov@gmail.com>
2019-03-06 20:52:10 +0100

    Remove all files

--- examples/with-return.js
+++
@@ -1,9 +0,0 @@
-const ksp = require('ksp')
-const run = ksp.runKsp
-
-ksp.createKsp('some thing', () => {
-    return 'Hello'
-})
-
-console.log(run('some thing'))
-// => "Hello"
--- examples/without-callback.js
+++
@@ -1,9 +0,0 @@
-const ksp = require('ksp')
-const run = ksp.runKsp
-
-ksp.createKsp('some thing', () => {
-    console.log('done!')
-})
-
-run('some thing')
-// => "done!"
